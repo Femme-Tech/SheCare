b412977df2715d08286e77cd0e64858265ed5697
Anushree Jain <143060171+AnushreeJ13@users.noreply.github.com>
2025-02-05 19:19:31 +0530

    Update main.py

--- PillPickr/webscraper/main.py
+++ PillPickr/webscraper/main.py
@@ -11,7 +11,49 @@ def home():
 @app.route('/survey', endpoint='survey')
 def survey():
     return render_template('survey.html')
+@app.route('/minipill', endpoint='minipill')
+def minipill():
+    return render_template('minipill.html')
 
+@app.route('/previfem', endpoint='previfem')
+def previfem():
+    return render_template('previfem.html')
+
+@app.route('/alesse', endpoint='alesse')
+def alesse():
+    return render_template('alesse.html')
+
+@app.route('/beyaz', endpoint='beyaz')
+def beyaz():
+    return render_template('beyaz.html')
+
+@app.route('/gianvi', endpoint='gianvi')
+def gianvi():
+    return render_template('gianvi.html')
+
+@app.route('/ocella', endpoint='ocella')
+def ocella():
+    return render_template('ocella.html')
+
+@app.route('/seasonique', endpoint='seasonique')
+def seasonique():
+    return render_template('seasonique.html')
+
+@app.route('/seasonale', endpoint='seasonale')
+def seasonale():
+    return render_template('seasonale.html')
+
+@app.route('/velivet', endpoint='velivet')
+def velivet():
+    return render_template('velivet.html')
+
+@app.route('/apri', endpoint='apri')
+def apri():
+    return render_template('apri.html')
+
+@app.route('/lybrel', endpoint='lybrel')
+def lybrel():
+    return render_template('lybrel.html')
 @app.route("/scrape")
 def scrape():
     name = "ortho micronor"
