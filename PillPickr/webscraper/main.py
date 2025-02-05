4f49195e7383b2ae6715d2ea6b1242976a80a3ab
Anushree Jain <143060171+AnushreeJ13@users.noreply.github.com>
2025-02-05 18:23:44 +0530

    Update main.py

--- PillPickr/webscraper/main.py
+++ PillPickr/webscraper/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route('/survey', name='survey')
+@app.route('/survey', endpoint='survey')
 def survey():
     return render_template('survey.html')
 
